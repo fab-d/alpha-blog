40b5540aec92d8a34675b9fc65ef70be28e6221c
Fabrizio <astaroth.f@gmail.com>
2016-05-02 10:37:39 +0000

    URL access and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
